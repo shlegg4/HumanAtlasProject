a79b6dec8d775584a962e6d908669d94cc9e5511
samuel <samuellegg2004@icloud.com>
2024-09-03 11:58:55 +0100

    Implemented dimensionality reduction using PCA

---
+++ src/data_processing/DimensionalityReduction.py
@@ -0,0 +1,15 @@
+from sklearn.decomposition import PCA
+import numpy as np
+
+class PCAProcessor:
+    def __init__(self, n_components=128):
+        self.pca = PCA(n_components=n_components)
+
+    def fit_transform(self, features):
+        reduced_features = self.pca.fit_transform(features)
+        explained_variance = np.sum(self.pca.explained_variance_ratio_)
+        print(f"Explained variance by {self.pca.n_components_} components: {explained_variance:.2f}")
+        return reduced_features
+
+    def transform(self, features):
+        return self.pca.transform(features)
